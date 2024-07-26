e243afcaf0ec533e2ca40236342f2ea66b7d61fd
Vladimir Chuev <vova1chuev1@gmail.com>
2024-07-26 16:38:03 +0300

    cookie

--- static/js/index.js
+++ static/js/index.js
@@ -22,3 +22,42 @@ document.getElementById('login-form').addEventListener('submit', async (event) =
         alert('Login failed!');
     }
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const cookieConsent = document.getElementById('cookieConsent');
+    const acceptCookies = document.getElementById('acceptCookies');
+    const declineCookies = document.getElementById('declineCookies');
+
+    if (!getCookie('cookieConsent')) {
+        cookieConsent.classList.add('show');
+    }
+
+    acceptCookies.addEventListener('click', function() {
+        setCookie('cookieConsent', 'true', 365);
+        cookieConsent.classList.remove('show');
+    });
+
+    declineCookies.addEventListener('click', function () {
+        setCookie('cookieConsent', 'true', 365);
+        cookieConsent.classList.remove('show');
+    });
+
+    function setCookie(name, value, days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        const expires = "expires=" + date.toUTCString();
+        document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    }
+
+    function getCookie(name) {
+        const nameEQ = name + "=";
+        const ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
+});
